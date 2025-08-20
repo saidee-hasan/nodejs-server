d1ed7035188ef03d7163a7e8c8aedf8d89aea6fe
saidee-hasan <mdsaideehasan@gmail.com>
2025-08-20 11:04:00 +0600

    product get

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,5 +1,10 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { products } from "../data/product";
 
 export  const handleProduct=async(req:IncomingMessage,res:ServerResponse)=>{
-    
+const url = req.url;
+res.end(JSON.stringify(products))
+
+
+
 }
